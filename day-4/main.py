41026a14639e5887acd27ca380b1ab1a174f1b0b
Catherine Oxley <catherine.oxley@live.co.uk>
2021-01-28 15:41:34 +0000

    refactor: day 4 of advent of code

--- day-4/main.py
+++ day-4/main.py
@@ -7,17 +7,16 @@ valid_passports = []
 with open(filename) as f:
     lines = f.read().split("\n\n")
     passports = []
-    for p in lines:
-        data = p.split("\n")
-        data = " ".join(data)
-        data = data.split()
-        passDict = {}
-        for d in data:
-            pair = d.split(":")
-            passDict[pair[0]] = pair[1]
-        keys = list(passDict.keys())
-        print(keys, sorted(required_items) == sorted(keys))
-        if (set(required_items).issubset(set(keys))):
-            valid_passports.append(passDict)
+
+for l in lines:
+    data = " ".join(l.split("\n")).split()
+    passDict = {}
+    for d in data:
+        pair = d.split(":")
+        passDict[pair[0]] = pair[1]
+    keys = list(passDict.keys())
+    print(keys, sorted(required_items) == sorted(keys))
+    if (set(required_items).issubset(set(keys))):
+        valid_passports.append(passDict)
 
 print(len(valid_passports))
